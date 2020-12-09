86b35cae1960ab95e4c0ac9d08c126f142a8b08e
Ahmed-aed <aeid767@gmail.com>
2020-12-09 13:32:56 +0200

    fix post and get method

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,9 @@
-const openForm=()=> {
-    console.log('open')
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  const closeForm=()=> {
-    console.log('close')
-    document.getElementById("myForm").style.display = "none";
-  }
+const openForm = () => {
+  console.log("open");
+  document.getElementById("myForm").style.display = "block";
+};
 
-
-  
+const closeForm = () => {
+  console.log("close");
+  document.getElementById("myForm").style.display = "none";
+};
--- server/database/queries/postArtist.js
+++ server/database/queries/postArtist.js
@@ -1,15 +1,16 @@
 const dbConnection = require("../config/connection");
 
-const postArtist1 = userData => {
-    console.log({userData})
-  const { name, yearOfBirth,nationality,workStyle,picture } = userData;
+const postArtist1 = (userData) => {
+  console.log({ userData });
+  const { name, yearOfBirth, nationality, workStyle, picture } = userData;
   const sql = {
-    text: "INSERT INTO artist (name, yearOfBirth,nationality,workStyle,picture ) VALUES ($1, $2,$3,$4,$5);",
-    values: [name,yearOfBirth,nationality,workStyle,picture ]
+    text:
+      "INSERT INTO artist (name, yearOfBirth,nationality,workStyle,picture ) VALUES ($1, $2,$3,$4,$5) returning *;",
+    values: [name, yearOfBirth, nationality, workStyle, picture],
   };
   return dbConnection.query(sql);
 };
 
 module.exports = {
-    postArtist1
+  postArtist1,
 };
